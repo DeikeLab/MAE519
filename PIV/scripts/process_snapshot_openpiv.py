d11e7a3e75c2014de3ada1c8f715a1f90802f597
Dan Ruth <danjruth@gmail.com>
2019-10-01 17:01:08 -0400

    test processing in matlab

--- PIV/scripts/process_snapshot_openpiv.py
+++ PIV/scripts/process_snapshot_openpiv.py
@@ -1,9 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 21 16:31:06 2019
-
-@author: ldeike
-"""
+'''
+Use this script to process a single snapshot (one a/b pair) with OpenPIV.
+'''
 
 # numpy and matplotlib are for math and making figures, respectively
 import numpy as np
@@ -18,11 +15,12 @@ import openpiv.validation
 import time
 
 # The folder where the example data is located
-folder = r'C:\Users\ldeike\Documents\MAE519_f2019\PIV\MAE519-piv-module\data\\'
+#folder = r'C:\Users\ldeike\Documents\MAE519_f2019\PIV\MAE519-piv-module\data\\'
+folder = r'C:\Users\danjr\Documents\Fluids Research\DeikeLab-GitHub\MAE519\PIV\data\\'
 
 # Set the spatial and temporal scaling
 dx = 80e-6 # the distance associated with one pixel, in meters
-dt = 1./1400 # the time between images acquired by the camera, in seconds
+dt = 1./1400 # the time between the "a" and "b" frames, in seconds
 
 # Load the a and b images, and make their datatype np.int32 
 frame_a  = openpiv.tools.imread(folder+'frame_000001.tif').astype(np.int32)
@@ -34,21 +32,22 @@ overlap = window_size/2.
 search_area = window_size
 
 # Compute the correlation to get the velocity field, in units of pixels/timestep
+print('Starting the PIV calculation...')
 t1 = time.time()
 u, v, sig2noise = openpiv.process.extended_search_area_piv( frame_a, frame_b, window_size=window_size, overlap=overlap, dt=1, search_area_size=search_area, sig2noise_method='peak2peak' )
-print('PIV calculation took '+str(time.time()-t1)+' seconds.')
+print('...PIV calculation took '+str(time.time()-t1)+' seconds.')
 
 # Filter out the points that had a low enough value of signal-to-noise ratio 
 u, v, mask = openpiv.validation.sig2noise_val( u, v, sig2noise, threshold = 1.3 )
 
-# Put the two components of velocity into a single 3d numpy array. moveaxis is used so the indexing is ff[row,column,velocity_component]
-ff = np.moveaxis(np.array([u,v]),0,-1)
+# Put the two components of velocity into a single 3d numpy array. moveaxis is used so the indexing is snapshot[row,column,velocity_component]
+snapshot = np.moveaxis(np.array([u,v]),0,-1)
 
 # "Scale" the velocity data by converting pixels/frame to meters/second
-ff = ff*(dx/dt)
+snapshot = snapshot*(dx/dt)
 
-# Calculate the speed by taking the norm alog the "component" axis of the flowfield array
-speed = np.linalg.norm(ff,axis=-1)
+# Calculate the speed by taking the norm alog the "component" axis of the snapshot array
+speed = np.linalg.norm(snapshot,axis=-1)
 
 # Show a map of the flow speed
 fig,ax = plt.subplots()
